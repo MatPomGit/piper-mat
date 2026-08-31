7f6de23d122e9a376cbeff58f7896407f587a951
Mateusz Pomianek <matpomianek@gmail.com>
2026-08-31 09:43:16 +0200

    git lfs

--- build_monotonic_align.sh
+++ build_monotonic_align.sh
@@ -3,12 +3,13 @@ set -eo pipefail
 
 this_dir="$( cd "$( dirname "$0" )" && pwd )"
 
+# Na Windowsie struktura folderów środowiska wirtualnego różni się od Linuksa – zamiast bin/ plik aktywacyjny znajduje się w katalogu Scripts/.
 if [ -d "${this_dir}/.venv" ]; then
-    source "${this_dir}/.venv/bin/activate"
+    source "${this_dir}/.venv/Scripts/activate"
 fi
 
 cd "${this_dir}/src/piper/train/vits/monotonic_align"
 mkdir -p monotonic_align
 rm -f core.c
 cythonize -i core.pyx
-mv core*.so monotonic_align/
+mv core*pyd monotonic_align/
